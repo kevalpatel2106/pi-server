2af8bb01aa8e172a038350d85ce2b7c9efb8b06d
kevalpatel2106 <kevalpatel2106@gmail.com>
2022-02-18 19:29:58 +0000

    Adding shellinabox and vs code server

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -18,13 +18,20 @@ sudo apt-get install -y  libffi-dev libssl-dev python3-dev python3 python3-pip
 sudo pip3 install docker-compose
 sudo systemctl enable docker    # Initialise docker on restart
 
+# Install shell in a box
+sudo apt install shellinabox
+
 # Memory monitoring permissions
 echo " systemd.unified_cgroup_hierarchy=0 cgroup_enable=memory cgroup_memory=1" >> /boot/cmdline.txt
+
+# Power saving
+vcgencmd display_power 0    # Turn off HDMI
 
 # Build and start stack
 docker-compose up -d
 sleep 10
 docker ps
 
-# Reboot
-sudo reboot
+echo "Steps remainign:
+1. Set up dynamic dns. If using duckDNS here is the guide: https://www.duckdns.org/install.jsp?tab=pi
+2. Reboot your PI"
